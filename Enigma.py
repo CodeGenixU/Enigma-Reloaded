f6e2853fc0c25118fe0b6930a8a8333fd9f7bc22
Utkarsh Kumar <sudhakumari3120@gmail.com>
2025-07-25 14:26:02 +0530

    Updating Settings

--- Enigma.py
+++ Enigma.py
@@ -2,32 +2,29 @@ import json
 
 class Enigma:
     def __init__(self, file):
-        with open(file, "r") as fh:
+        with open(file, "r", encoding = "utf-8") as fh:
             key = json.load(fh)
-            self.r1 = key["r1"]
-            self.r2 = key["r2"]
-            self.r3 = key["r3"]
-            self.plugs = key["plugs"]
-            self.iteration = key["iteration"]
-            self.n = key["n"]
+            self.number_of_rotors = key["setting"]["number_of_rotor"]
+            self.iteration = key["setting"]["iteration"]
+            self.n = key["setting"]["n"]
+            self.rotor = []
+            for i in key["setting"]["sequence_of_rotor"].split(">"):
+                self.rotor.append(key[i])
+            self.plugs = key["setting"]["plugs"]
             self.characters = key["characters"]
-        
+
+            if self.iteration != 0:
+                for i in range(self.number_of_rotors):
+                    rot_count = (self.iteration // (self.n)**i) % 100
+                    self.rotor[i] = self.rotor[i][rot_count:] + self.rotor[i][:rot_count]
     @staticmethod
     def __rotate(l):
-        l.append(l[0])
-        l.pop(0)
+        l.append(l.pop(0))
         return l
     
     
     def __plugin(self, char):
-        if len(self.plugs) != 0:
-            for i in self.plugs:
-                if char in i:
-                    return i[1 - i.index(char)]
-            else :
-                return char
-        else:
-            return char
+        return self.plugs.get(char, char)
     
     def __prerotor(self, char):
         return self.characters.index(char)
@@ -38,9 +35,11 @@ class Enigma:
     def __encode(self, char):
         c = self.__plugin(char)
         se = self.__prerotor(c)
-        ic = self.r3[self.r2[self.r1[se]]]
-        refector = 100 - ic
-        fc = self.r1[self.r2[self.r3[refector]]]
+        for i in self.rotor:
+            se = i[se]
+        fc = 99 - se
+        for i in self.rotor.copy()[::-1]:
+            fc = i.index(fc)
         fe = self.__postrotor(fc)
         e = self.__plugin(fe)
         return e
@@ -48,11 +47,9 @@ class Enigma:
     def main(self, character):
         code = self.__encode(character)
         self.iteration += 1
-        self.__rotate(self.r1)
-        if self.iteration % self.n == 0:
-            self.__rotate(self.r2)
-        
-        if self.iteration % (self.n)**2 == 0:
-            self.__rotate(self.r3)
+        for i in range(self.number_of_rotors):
+            if self.iteration % (self.n)**i == 0:
+                self.__rotate(self.rotor[i])
         return code
+
 
